eee53fbbf7e459fbf45eb50f05455b9268d0793d
korynewton <korynewtondev@gmail.com>
2019-02-04 15:14:18 -0800

    mobile first design complete

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 //hamburger button
 let hamburgerBtn = document.querySelector('.navbar .buttons')
 
-let loginSignUp =  document.querySelectorAll('.logo a')
+let loginSignUp =  document.querySelectorAll('.no-show')
 
 //add event listener to hamburger button
 hamburgerBtn.addEventListener('click', event => {
