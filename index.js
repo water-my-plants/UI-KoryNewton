187af976c220bd7c5d290b4d445fdd47852df256
korynewton <korynewtondev@gmail.com>
2019-02-06 09:36:56 -0800

    mouseover/mouseout event listeners added to Learn More button

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ let loginSignUp =  document.querySelectorAll('.no-show')
 hamburgerBtn.addEventListener('click', event => {
     loginSignUp.forEach(item => item.classList.toggle('no-show'))
 })
+
+
+//Event listener for "Learn More" button:
+let learnMoreBtn = document.querySelector('.learnMore-btn')
+
+learnMoreBtn.addEventListener('mouseover', event => {
+    event.target.style.backgroundColor = '#5AAB61';
+    event.target.style.color = 'white'
+})
+
+learnMoreBtn.addEventListener('mouseout', event => {
+    event.target.style.backgroundColor = 'white';
+    event.target.style.color = 'black'
+})
+
